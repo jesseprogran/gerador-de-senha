104ec8771ca435ac360df45ca647109f1fb5391f
Jessé Silva <jessepescador22@gmail.com>
2023-04-04 20:40:32 -0300

    feito a fubção de copiar o elemento de senha

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+const elInput = document.querySelector("#password");
+let passwordLength = 20
+
 function generatePassword() {
   const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ1234567889?!@&*$()[]";
 
   let password = ""
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     const numberRandom = Math.floor(Math.random() * chars.length);
     password += chars.substring(numberRandom, numberRandom + 1);
   }
 
-  const elInput = document.querySelector("#password");
   elInput.value = password
 
 }
 
-generatePassword()
+function copy() {
+  navigator.clipboard.writeText(elInput.value)
+}
+
+const passwordLengthEl = document.querySelector("#password-length");
+passwordLengthEl.addEventListener("input", function() {
+  passwordLength = passwordLengthEl.value
+  generatePassword();
+})
+
+const copyButtonEl = document.querySelector("#button");
+copyButtonEl.addEventListener("click", copy);
+
+generatePassword() 
